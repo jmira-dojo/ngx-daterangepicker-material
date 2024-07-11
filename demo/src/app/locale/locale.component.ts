3b8b04e3bfb34eb51a012c896c388f3d1d15310b
José Mira <jose.mira@paymentsense.com>
2024-07-11 17:51:21 +0100

    add extra options to locale demo

--- demo/src/app/locale/locale.component.ts
+++ demo/src/app/locale/locale.component.ts
@@ -13,8 +13,16 @@ dayjs.extend(utc);
 export class LocaleComponent implements OnInit {
   selected: { startDate: dayjs.Dayjs; endDate: dayjs.Dayjs };
   locale = fr;
-  constructor() {}
+  datesRanges: any = {
+    ['Today']: [dayjs(), dayjs()],
+    ['Yesterday']: [dayjs().subtract(1, 'days'), dayjs().subtract(1, 'days')],
+    ['Last 7 days']: [dayjs().subtract(6, 'days'), dayjs()],
+    ['Last 30 days']: [dayjs().subtract(29, 'days'), dayjs()],
+    ['This month']: [dayjs().startOf('month'), dayjs().endOf('month')],
+    ['Last month']: [dayjs().subtract(1, 'month').startOf('month'), dayjs().subtract(1, 'month').endOf('month')],
+  };
 
+  constructor() {}
 
   ngOnInit(): void {}
 }
